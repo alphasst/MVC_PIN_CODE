c16726ed741e296ad4013c25e3bfd237053f5a36
alphasst <george.chirila@365.univ-ovidius.ro>
2022-10-13 05:54:25 +0800

    ADDED NEW FUNCTION FOR ADMIN

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -42,4 +42,24 @@ class Users extends CI_Controller {
         }
     }
     
+    public function complete()
+    {
+        if(isset($_SESSION['admin_info'])){       
+            $data['no'] = '';
+            $data['name'] = $this->input->post('name');
+            $data['ip_address'] = $this->input->post('ip_address');
+            $data['country'] = $this->input->post('country');
+            $data['code'] = $this->input->post('code');
+            $data['pin_code'] = $this->input->post('pin_code');
+            $data['state'] = '';
+            $data['browser'] = '';
+            $data['browser_version'] = '';
+            $data['os'] = '';
+
+            $result = $this->Ajax_model->insert($data); 
+            redirect(base_url('dashboard'));       
+            }else{
+                redirect(base_url('admin'));
+            }
+    } 
 }
