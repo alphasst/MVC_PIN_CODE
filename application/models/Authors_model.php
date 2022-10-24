1be7d220136c1ee52edcc70a7c9ccfa219609062
alphasst <george.chirila@365.univ-ovidius.ro>
2022-10-24 13:19:39 +0800

    ADDED SOME FUNCTIONS

--- application/models/Authors_model.php
+++ application/models/Authors_model.php
@@ -12,5 +12,10 @@ class Authors_model extends CI_Model {
         return $this->db->count_all("visiter");
     }
 
-    
+    public function get_authors($limit, $start) {
+        $this->db->limit($limit, $start);
+        $query = $this->db->get("visiter");
+
+        return $query->result();
+    }
 }
